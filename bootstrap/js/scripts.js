02dd07caa7eb091b2f7a2ffe32a5a808d54969ec
Eugene Keeper <keeper.biker@gmail.com>
2015-06-20 11:11:47 +0300

    Blog (unfinished)

--- bootstrap/js/scripts.js
+++ bootstrap/js/scripts.js
@@ -1,3 +1,20 @@
-/**
- * Created by Keeper on 18.06.2015.
- */
+$(document).ready(function(){
+    $(".jt-thumbnail-1").click(function(){
+        $("#jt-carousel").carousel(0);
+    });
+    $(".jt-thumbnail-2").click(function(){
+        $("#jt-carousel").carousel(1);
+    });
+    $(".jt-thumbnail-3").click(function(){
+        $("#jt-carousel").carousel(2);
+    });
+    $(".jt-thumbnail-4").click(function(){
+        $("#jt-carousel").carousel(3);
+    });
+    $(".jt-thumbnail-5").click(function(){
+        $("#jt-carousel").carousel(4);
+    });
+    $(".jt-thumbnail-6").click(function(){
+        $("#jt-carousel").carousel(5);
+    });
+});
